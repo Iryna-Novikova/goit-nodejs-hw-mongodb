690455bc25857361bc9a5faf70662f53ae03c7eb
Iryna_Novikova <iris_vs@ukr.net>
2025-10-26 02:15:20 +0300

    hw7 done

--- src/services/auth.js
+++ src/services/auth.js
@@ -134,12 +134,12 @@ export const requestResetToken = async (email) => {
       html,
     });
   } catch (err) {
-    // if (err instanceof Error)
-    throw createHttpError(
-      500,
-      `Failed to send the email, please try again later.`,
-    );
-    // throw err;
+    if (err instanceof Error)
+      throw createHttpError(
+        500,
+        `Failed to send the email, please try again later.`,
+      );
+    throw err;
   }
 };
 
